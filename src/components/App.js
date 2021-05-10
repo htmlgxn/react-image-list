4854102023fe3aca8dd416aa3c3fd8e1bee440ba
Ben Chitty <htmlgxn@protonmail.com>
2021-05-09 21:51:09 -0300

    Created App.js and SearchInput.js component; Semantic UI imported

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchInput from './SearchInput';
+
+const App = () => {
+    return (
+        <div>
+            <SearchInput />
+        </div>
+    )
+}
+
+export default App;
