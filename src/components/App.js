fab2e53ede86a41e4761d9be0e6431de104fef56
Ben Chitty <htmlgxn@protonmail.com>
2021-05-10 20:57:25 -0300

    Created ImageList.js component; Added Pixabay API and app displays searched images

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
 import SearchInput from './SearchInput';
+import ImageList from './ImageList';
 
-const App = () => {
-    return (
-        <div>
-            <SearchInput />
-        </div>
-    )
+class App extends React.Component {
+
+    state = { images: [] }
+
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=21545382-3f492836205ab4207b0c87b9e&q=${encodeURIComponent(entry)}&image_type=photo`)
+        this.setState({images: response.data.hits})
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{marginTop: '30px'}} >
+                <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
+            </div>
+        )
+    }
 }
 
 export default App;
